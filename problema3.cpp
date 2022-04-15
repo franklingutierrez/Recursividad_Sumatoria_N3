4224f45aea3c89bbdc187095ac908ca1668bcad6
guiratec <fgutierrezarizaca@gmail.com>
2022-04-15 15:53:21 -0500

    proceso

--- problema3.cpp
+++ problema3.cpp
@@ -1,6 +1,23 @@
+/*5. Mostrar la sumatoria de: suma=1+3+4+7+11+.....+Xn
+Autor --> Franklin Hiustong Gutierrez Arizaca*/
 #include <iostream>
-
+#include <conio.h>
+using namespace std;
+//int suma(int);
+int sumatoria(int a, int b, int limite){
+    int n = a + b;
+    cout<<n<<" ";
+    if(n>=limite){
+        return 0;
+    }
+    return sumatoria(b,n,limite);
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int x;
+    cout<<"Ingresa la cantidad [mayor a 0] para calcular la suma de la sucesion [1+3+4+7+11+.....+Xn]: ";
+    cin>>x;
+    cout<<"1 "<<"3 ";
+    sumatoria(1,3,x);
+    cin.get();
     return 0;
 }
